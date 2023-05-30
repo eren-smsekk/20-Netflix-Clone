ea4366541c0ed5866a9b093ddf6cfea9349ca015
eren.simsek@wsrlife.com <eren.simsek@wsrlife.com>
2023-05-30 14:24:57 +0300

    netflix-clone

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-
-
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         let scroll = $(window).scrollTop();
-//         if(scroll > 100) {
-//             $(".netflix-navbar").css("background", "#0c0c0c");
-//         } else {
-//             $(".netflix-navbar").css("background", "transparent");
-//         }
-//     })
-// })
-
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("scroll", function() {
-        var scroll = window.scrollTop;
-        if (scroll > 100) {
+        var scroll = window;
+        console.log(scroll)
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
             document.querySelector(".netflix-navbar").style.background = "#0c0c0c";
         } else {
             document.querySelector(".netflix-navbar").style.background = "transparent";
